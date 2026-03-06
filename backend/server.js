76d3897703445f5303d7b1742a7b74493ccc7f70
KavinduAnj <wkanjana1590@gmail.com>
2026-03-06 23:18:35 +0530

    Database connect

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,13 @@
+require('dotenv').config();
 const express = require("express");
+const connectDB = require('./db'); // Import the DB connection function
 
 const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("ResQNow backend running");
@@ -8,4 +15,9 @@ app.get("/", (req, res) => {
 
 app.listen(5000, () => {
   console.log("Server running on port 5000");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
