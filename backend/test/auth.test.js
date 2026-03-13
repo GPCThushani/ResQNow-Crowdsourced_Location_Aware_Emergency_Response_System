d2598329f65ddb2c8f2d702a773549b72f21c82a
DThawi <dthawi0319@gmail.com>
2026-03-13 22:30:33 +0530

    test: add unit tests for incident API

--- backend/test/auth.test.js
+++ backend/test/auth.test.js
@@ -6,16 +6,20 @@ describe("Authentication API Tests", () => {
 
   const testUser = {
     name: "Test User",
-    email: "test${Date.now()}@example.com",
+    email: 'test${Date.now()}@example.com',
     password: "123456",
     role: "Citizen",
-    contact_number: "0771234567"
+  contact_number: "0771234567"
+    
   };
+
+
 
   test("Register user", async () => {
     const res = await request(app)
       .post("/api/auth/register")
       .send(testUser);
+      console.log("REGISTER RESPONSE:", res.body); 
 
     expect(res.statusCode).toBe(201);
   });
@@ -31,9 +35,42 @@ describe("Authentication API Tests", () => {
     expect(res.statusCode).toBe(200);
   });
 
+  test("Register duplicate user", async () => {
+  const res = await request(app)
+    .post("/api/auth/register")
+    .send(testUser);
+
+  expect(res.statusCode).toBe(400);
 });
 
-// 👇 ADD THIS PART HERE (bottom of file)
+test("Login with wrong password", async () => {
+
+  const res = await request(app)
+    .post("/api/auth/login")
+    .send({
+      email: testUser.email,
+      password: "wrongpassword"
+    });
+
+  expect(res.statusCode).toBe(400);
+
+});
+
+test("Login without email", async () => {
+
+  const res = await request(app)
+    .post("/api/auth/login")
+    .send({
+      password: "123456"
+    });
+
+  expect(res.statusCode).toBe(400);
+
+});
+
+});
+
+//  ADD THIS PART HERE (bottom of file)
 
 afterAll(async () => {
   await mongoose.connection.close();
